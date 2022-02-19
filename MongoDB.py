2a4ed552981b2cdf2eb73d8cf2e0aa3d4442baa9
Salim Al Kharsa <61260042+SalimAlkharsa@users.noreply.github.com>
2022-02-19 14:28:52 -0600

    Added read_to_db

--- MongoDB.py
+++ MongoDB.py
@@ -15,5 +15,13 @@ def upload_data_to_db(file, BASE):
   df.to_json('yourjson.json')                               # saving to json file
   jdf = open('yourjson.json').read()                        # loading the json file 
   data = json.loads(jdf)
-  db[BASE].insert_many([data])
+  db[BASE].insert_many(data)
   pass
+
+def read_db_to_df(BASE, query ={}):
+  password = input("Enter the db pw: ")
+  client = pymongo.MongoClient("mongodb+srv://TransferDatabase:"+password+"@cluster0.pncv8.mongodb.net/test?retryWrites=true&w=majority")
+  db = client.test
+  cursor = db[BASE].find(query)
+  df =  pd.DataFrame(list(cursor))
+  return df
