e9b4ed4af333030a1ea7282fca272fbe86ae3403
Salim Al Kharsa <61260042+SalimAlkharsa@users.noreply.github.com>
2022-02-19 00:43:30 -0600

    Create MongoDB.py
    Created the very initial undocumented code

---
+++ MongoDB.py
@@ -0,0 +1,14 @@
+import json
+import pymongo
+import pandas as pd
+
+def upload_data_to_db(file, BASE):
+  password = input("Enter the db pw: ")
+  client = pymongo.MongoClient("mongodb+srv://TransferDatabase:"+password+"@cluster0.pncv8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+  db = client.test
+  df = pd.read_csv(file+'.csv',encoding = 'ISO-8859-1')   # loading csv file
+  df.to_json('yourjson.json')                               # saving to json file
+  jdf = open('yourjson.json').read()                        # loading the json file 
+  data = json.loads(jdf)
+  db.BASE.insert_many(data)
+  pass
